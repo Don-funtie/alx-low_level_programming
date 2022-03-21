3fcbbe6d553ad7c31b7b6b9ce67530187fc2d5fb
Don-funtie <99618093+Don-funtie@users.noreply.github.com>
2022-03-21 12:11:28 -0400

    Update

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,23 +1,26 @@
- ​#​include​ ​"​main.h​" 
-  
- ​/*​* 
- ​ * puts2 - prints one char out of 2 of a string. 
- ​ * @str: input string. 
- ​ * Return: no return. 
- ​ ​*/ 
- ​void​ ​puts2​(​char​ *str) 
- ​{ 
- ​        ​int​ count = ​0​; 
-  
- ​        ​while​ (count >= ​0​) 
- ​        { 
- ​                ​if​ (str[count] == ​'​\0​'​) 
- ​                { 
- ​                        ​_putchar​(​'​\n​'​); 
- ​                        ​break​; 
- ​                } 
- ​                ​if​ (count % ​2​ == ​0​) 
- ​                        ​_putchar​(str[count]); 
- ​                count++; 
- ​        } 
- ​}
+#include "holberton.h"
+
+ /**
+ * puts2 - main function.
+ * @str: Pointer, the string to print.
+ *
+ * Description: This function prints every other character of a string
+ * (divisible by two)
+ * Return: none.
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	while (str[i])
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+
+		i++;
+	}
+	_putchar('\n');
+}
