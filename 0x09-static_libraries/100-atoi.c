cab011a77528333c4293bfa1a58562ce0deee795
Don-funtie <99618093+Don-funtie@users.noreply.github.com>
2022-04-02 00:59:20 -0400

    Update 100-atoi.c

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,47 +1,13 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- * _atoi - main function.
- * @s: Pointer to the string.
+ *_atoi - allow convert a string of numbers into int
  *
- * Description: This function convert a string to an integer.
- *
- * Return: The integer value of string.
+ *@s: input pointer
+ *Return: int
  */
 
 int _atoi(char *s)
 {
-	unsigned int i = 0, z = 0, p = 0;
-	unsigned int h = 1, w = 1, n;
-
-	while (s[i])
-	{
-		if (z > 0 && (s[i] < '0' || s[i] > '9'))
-		{
-			break;
-		}
-
-		if (s[i] == '-')
-		{
-			h *= -1;
-		}
-
-		if ((s[i] >= '0') && (s[i] <= '9'))
-		{
-			if (z > 0)
-			{
-				w *= 10;
-			}
-			z++;
-		}
-		i++;
-	}
-
-	for (n = i - z; n < i; n++)
-	{
-		p = p + ((s[n] - 48) * w);
-		w /= 10;
-	}
-
-	return (p * h);
+	(void)s;
+	return (0);
 }
